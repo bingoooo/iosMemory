5fb03d07994a4dd3b99bf6c7069c20f46f400deb
Benjamin Dant <benjamindant@gmail.com>
2016-10-19 15:17:41 +0200

    First view : Difficulty choice

--- memory/ViewController.swift
+++ memory/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    enum Difficulty {
+        case Easy, Medium, Hard
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        setup()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +29,49 @@ class ViewController: UIViewController {
 
 }
 
+private extension ViewController {
+    func setup() {
+        view.backgroundColor = UIColor.white
+        
+        buildButtonWithCenter(center: CGPoint(x: view.center.x, y: view.center.y/2.0), title: "Easy", color: UIColor.green, action: #selector(ViewController.onEasyTaped))
+        buildButtonWithCenter(center: CGPoint(x: view.center.x, y: view.center.y), title: "Medium", color: UIColor.yellow, action: #selector(ViewController.onMediumTaped))
+        buildButtonWithCenter(center: CGPoint(x: view.center.x, y: view.center.y * 3.0/2.0), title: "Hard", color: UIColor.red, action: #selector(ViewController.onHardTaped))
+    }
+    
+    func buildButtonWithCenter(center: CGPoint, title: String, color: UIColor, action: Selector){
+        let button = UIButton()
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(UIColor.black, for: .normal)
+        button.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 200, height: 50))
+        button.center = center
+        button.backgroundColor = color
+        
+        button.addTarget(self, action: action, for: UIControlEvents.touchUpInside)
+        view.addSubview(button)
+    }
+}
+
+extension ViewController {
+    func onEasyTaped() {
+        newGameDifficulty(difficulty: .Easy)
+    }
+    
+    func onMediumTaped() {
+        newGameDifficulty(difficulty: .Medium)
+    }
+    
+    func onHardTaped() {
+        newGameDifficulty(difficulty: .Hard)
+    }
+    
+    func newGameDifficulty(difficulty: Difficulty){
+        switch difficulty {
+        case .Easy:
+            print("Easy")
+        case .Medium:
+            print("Medium")
+        case .Hard:
+            print("Hard")
+        }
+    }
+}
