ba0c4f2529969ff7568cbe6a789cdf0ec108ca71
Benjamin Dant <benjamindant@gmail.com>
2016-10-19 14:36:14 +0200

    Initial Commit

---
+++ memory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  memory
+//
+//  Created by Benjamin on 19/10/2016.
+//  Copyright © 2016 Benjamin Dant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
